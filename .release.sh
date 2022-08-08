b3c6443b8ee532aafa27e971deec6aa0d9afed5b
James Frowen <jamesfrowendev@gmail.com>
2022-08-08 18:02:40 +0100

    chore: repo and CI config

---
+++ .release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Running release script with [SOURCE_PATH=${SOURCE_PATH}, TARGET_PATH=${TARGET_PATH}, args=$@]"
+
+VER=$(echo $1 | sed 's/-[a-z]*//g')
+sed -i -e '/AssemblyVersion/s/\".*\"/\"'$VER'\"/' \
+    ${SOURCE_PATH}/Runtime/AssemblyInfo.cs
+
+unity-packer pack Mirage.EpicSocket.unitypackage \
+    ${SOURCE_PATH} ${TARGET_PATH}
